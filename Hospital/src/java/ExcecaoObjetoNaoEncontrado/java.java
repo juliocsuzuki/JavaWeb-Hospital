3de34004eaaf65c9b4aad8b833620039b83bc715
juliocsuzuki <50452741+juliocsuzuki@users.noreply.github.com>
2019-10-09 10:17:03 -0300

    Add files via upload

---
+++ Hospital/src/java/ExcecaoObjetoNaoEncontrado/java.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ExcecaoObjetoNaoEncontrado;
+
+/**
+ *
+ * @author CC08452737955
+ */
+public class java {
+    
+}
---
+++ Hospital/src/java/util/JPAUtil.java
@@ -0,0 +1,14 @@
+package util;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+public class JPAUtil {
+    private static final EntityManagerFactory FACTORY;
+    
+    static{
+        FACTORY = Persistence.createEntityManagerFactory("HospitalPU");
+    }
+    public static EntityManager getEntityManager(){
+        return FACTORY.createEntityManager();
+    }
+}
